f3d11a252783872cbb4d1cb579b232d63e023793
Mir Reza <mukit1400@gmail.com>
2020-12-29 11:38:27 -0500

    master

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,11 +21,25 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: [
+        trackingId: [
           "G-GLTQHF8QPS",
-          "",
-          "",
         ],
+
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+
       },
     },
     {
